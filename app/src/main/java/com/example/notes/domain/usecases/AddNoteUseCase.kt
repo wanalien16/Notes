2164ac75172eaf6003522466062e2094423ad6fc
anilsai <anilsai.damayapally16@gmail.com>
2024-10-08 13:15:24 +0530

    fixed errors related to di in the usecases.

--- app/src/main/java/com/example/notes/domain/usecases/AddNoteUseCase.kt
+++ app/src/main/java/com/example/notes/domain/usecases/AddNoteUseCase.kt
@@ -2,8 +2,9 @@ package com.example.notes.domain.usecases
 
 import com.example.notes.domain.Note
 import com.example.notes.domain.NoteRepository
+import javax.inject.Inject
 
-class AddNoteUseCase(private val repository: NoteRepository) {
+class AddNoteUseCase @Inject constructor(private val repository: NoteRepository) {
     suspend operator fun invoke(note: Note){
         return repository.addNote(note)
     }
--- app/src/main/java/com/example/notes/domain/usecases/GetAllNotesUseCase.kt
+++ app/src/main/java/com/example/notes/domain/usecases/GetAllNotesUseCase.kt
@@ -2,8 +2,9 @@ package com.example.notes.domain.usecases
 
 import com.example.notes.domain.Note
 import com.example.notes.domain.NoteRepository
+import javax.inject.Inject
 
-class GetAllNotesUseCase(private val repository: NoteRepository) {
+class GetAllNotesUseCase @Inject constructor(private val repository: NoteRepository) {
     suspend operator fun invoke(): List<Note>{
         return repository.getAllNotes()
     }
